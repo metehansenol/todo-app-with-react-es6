f57c76e2ba9319b36d6436ca43ae8947f3b958ff
Metehan Senol <metehansenol@gmail.com>
2020-05-05 23:07:12 +0300

    creating new item

--- src/App.js
+++ src/App.js
@@ -5,16 +5,38 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      items: []
+      items: [],
+      title: ''
     };
 
     this.todoApiUrl = 'http://localhost:5000';
+
+    this.createNewItem = this.createNewItem.bind(this);
   }
 
   componentDidMount() {
     fetch(`${this.todoApiUrl}/items`)
       .then(response => response.json())
-      .then(items => this.setState({items}))
+      .then(items => this.setState({items}));
+  }
+
+  createNewItem() {
+    fetch(`${this.todoApiUrl}/items`, {
+      method: 'POST',
+      headers: new Headers({'Content-Type': 'application/json; charset=utf-8'}),
+      body: JSON.stringify({title: this.state.title})
+    }).then(response => {
+      if (!response.ok) {
+        alert('An error occurred while creating new item');
+      } else {
+        return response.json();
+      }
+    }).then(createdItem => {
+      this.setState(prevState => ({
+        items: [...prevState.items, {id: createdItem.id, title: createdItem.title}],
+        title: ''
+      }));
+    });
   }
 
   render() {
@@ -27,6 +49,11 @@ class App extends React.Component {
               this.state.items.map(item => <li key={item.id}>{item.title}</li>)
             }
           </ul>
+
+          <div>
+            Title: <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+            <button onClick={this.createNewItem}>Add New</button>
+          </div>
         </div>
     );
   }
