c8f4244f7fb94e453b41b15bf86ca3fc14869362
Metehan Senol <metehansenol@gmail.com>
2020-05-06 00:35:31 +0300

    updating and deleting selected item

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ class App extends React.Component {
 
     this.state = {
       items: [],
-      title: ''
+      title: '',
+      selectedItemId: null
     };
 
     this.todoApiUrl = 'http://localhost:5000';
 
     this.createNewItem = this.createNewItem.bind(this);
+    this.updateSelectedItem = this.updateSelectedItem.bind(this);
+    this.deleteSelectedItem = this.deleteSelectedItem.bind(this);
   }
 
   componentDidMount() {
@@ -39,6 +42,42 @@ class App extends React.Component {
     });
   }
 
+  updateSelectedItem() {
+    fetch(`${this.todoApiUrl}/items/${this.state.selectedItemId}`, {
+      method: 'PUT',
+      headers: new Headers({'Content-Type': 'application/json; charset=utf-8'}),
+      body: JSON.stringify({title: this.state.title})
+    }).then(response => {
+      if (!response.ok) {
+        alert('An error occurred while updating item');
+      } else {
+        const {items, title, selectedItemId} = this.state;
+        const index = items.findIndex((item) => item.id === selectedItemId);
+        items.splice(index, 1, {id: selectedItemId, title: title});
+        this.setState({items: items});
+      }
+    });
+  }
+
+  deleteSelectedItem() {
+    fetch(`${this.todoApiUrl}/items/${this.state.selectedItemId}`, {
+      method: 'DELETE'
+    }).then(response => {
+      if (!response.ok) {
+        alert('An error occurred while updating item');
+      } else {
+        const {items, selectedItemId} = this.state;
+        const index = items.findIndex((item) => item.id === selectedItemId);
+        items.splice(index, 1);
+        this.setState({
+          items: items,
+          title: '',
+          selectedItemId: null
+        });
+      }
+    });
+  }
+
   render() {
     return (
         <div className="app-container">
@@ -46,13 +85,21 @@ class App extends React.Component {
 
           <ul>
             {
-              this.state.items.map(item => <li key={item.id}>{item.title}</li>)
+              this.state.items.map(item => <li key={item.id}>
+                <a href='#'
+                   onClick={() => this.setState({selectedItemId: item.id, title: item.title})}
+                   className={this.state.selectedItemId === item.id ? 'selected' : ''}>
+                  {item.title}
+                </a>
+              </li>)
             }
           </ul>
 
           <div>
             Title: <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
             <button onClick={this.createNewItem}>Add New</button>
+            <button onClick={this.updateSelectedItem}>Update</button>
+            <button onClick={this.deleteSelectedItem}>Delete</button>
           </div>
         </div>
     );
